2443e84de3cafaf6309349f14ea20244be370bde
Zen <masliyoleksandr@gmail.com>
2025-05-08 21:51:51 +0300

    Writing first code
    Trying to make login and registration pages

--- reg.php
+++ reg.php
@@ -3,11 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Asssignet</title>
+    <title>Register</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-<h1> Ввійдіть в систему: </h1>  
-<a class = "btn1" href="/login.php"> Ввійти</a>
+    
 </body>
 </html>
